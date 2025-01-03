2d98f1f1c587af5cc0847e1da8617960d2bd2345
soul <sangig99@gmail.com>
2025-01-03 17:11:54 +0100

    added better time mesurament

--- include/Utility.h
+++ include/Utility.h
@@ -1,34 +1,40 @@
-#include <iostream>
-#include <vector>
 #include <opencv2/opencv.hpp>
+#include <iostream>
+#include <fstream>
 #include <chrono>
-#include <numeric>
-#include <cmath>
+#include <vector>
+#include <string>
 
-void equalizeHistogram(uchar* pdata, int width, int height, int max_val = 255) {
-    int total = width * height;
-    const int n_bins = max_val + 1;
+void computeHistogram(const cv::Mat& img, std::vector<int>& histogram) {
+    histogram.assign(256, 0);
+    for (int i = 0; i < img.rows; ++i) {
+        for (int j = 0; j < img.cols; ++j) {
+            histogram[img.at<uchar>(i, j)]++;
+        }
+    }
+}
 
-    // Compute histogram and initialize LUT
-    int hist[n_bins] = {0};
-    int lut[n_bins] = {0};
+void computeCDF(const std::vector<int>& histogram, std::vector<int>& cdf) {
+    cdf.assign(256, 0);
+    cdf[0] = histogram[0];
+    for (size_t i = 1; i < histogram.size(); ++i) {
+        cdf[i] = cdf[i - 1] + histogram[i];
+    }
+}
 
-    // Histogram computation
-    for (int i = 0; i < total; ++i) {
-        hist[pdata[i]]++;
+void equalizeImage(cv::Mat& img, const std::vector<int>& cdf) {
+    int totalPixels = img.rows * img.cols;
+    int minCDF = *std::min_element(cdf.begin(), cdf.end());
+    std::vector<uchar> lut(256);
+
+    #pragma omp parallel for
+    for (size_t i = 0; i < cdf.size(); ++i) {
+        lut[i] = static_cast<uchar>(((cdf[i] - minCDF) * 255) / (totalPixels - minCDF));
     }
 
-    // Build LUT from cumulative histogram
-    int sum = 0;
-    float scale = (n_bins - 1.f) / total;
-
-    for (int i = 0; i < n_bins; ++i) {
-        sum += hist[i];
-        lut[i] = std::max(0, std::min(int(round(sum * scale)), max_val));
-    }
-
-    // Apply equalization
-    for (int i = 0; i < total; ++i) {
-        pdata[i] = lut[pdata[i]];
+    for (int i = 0; i < img.rows; ++i) {
+        for (int j = 0; j < img.cols; ++j) {
+            img.at<uchar>(i, j) = lut[img.at<uchar>(i, j)];
+        }
     }
 }

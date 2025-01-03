0dee12cb1b9c4ec30f6ba37c3d1c097aaaecd4c4
soul <sangig99@gmail.com>
2025-01-03 08:30:13 +0100

    not saving?

--- cuda_main.cu
+++ cuda_main.cu
@@ -1,46 +1,16 @@
-#include <iostream>
+#include "Utility Cuda.cu"
 #include <opencv2/opencv.hpp>
-#include <cuda_runtime.h>
-#include <vector>
-#include <utility>
-
-__global__ void compute_histogram(const unsigned char* d_input, int* d_hist, int width, int height) {
-    int idx = threadIdx.x + blockIdx.x * blockDim.x;
-    if (idx < width * height) {
-        atomicAdd(&d_hist[d_input[idx]], 1);
-    }
-}
-
-__global__ void compute_cdf(int* d_hist, int* d_cdf, int total_pixels) {
-    int idx = threadIdx.x + blockIdx.x * blockDim.x;
-    if (idx < 256) {
-        d_cdf[idx] = 0;
-        for (int i = 0; i <= idx; ++i) {
-            d_cdf[idx] += d_hist[i];
-        }
-        d_cdf[idx] = (d_cdf[idx] * 255) / total_pixels;
-    }
-}
-
-__global__ void equalize_image(unsigned char* d_output, const unsigned char* d_input, const int* d_cdf, int width, int height) {
-    int idx = threadIdx.x + blockIdx.x * blockDim.x;
-    if (idx < width * height) {
-        d_output[idx] = d_cdf[d_input[idx]];
-    }
-}
 
 int main() {
     std::string inputPath = "../images/img1.bmp";
     std::string outputPath = "../outputs/";
 
-    // Read input image
     cv::Mat img = cv::imread(inputPath, cv::IMREAD_GRAYSCALE);
     if (img.empty()) {
         std::cerr << "Error: Could not load image at " << inputPath << std::endl;
         return -1;
     }
 
-    // Define sizes to test
     std::vector<std::pair<int, int>> sizes = {
         {128, 128}, {256, 256}, {512, 512}, {1024, 1024}, {2048, 2048}
     };
@@ -48,7 +18,6 @@ int main() {
     for (auto size : sizes) {
         std::cout << "Processing image with size: " << size.first << "x" << size.second << std::endl;
 
-        // Resize the image
         cv::Mat resized_img;
         cv::resize(img, resized_img, cv::Size(size.first, size.second));
 
@@ -62,13 +31,10 @@ int main() {
         cudaMalloc(&d_hist, 256 * sizeof(int));
         cudaMalloc(&d_cdf, 256 * sizeof(int));
 
-        // Copy resized image to device 
         cudaMemcpy(d_input, resized_img.data, width * height * sizeof(unsigned char), cudaMemcpyHostToDevice);
 
-        // Init histogram
         cudaMemset(d_hist, 0, 256 * sizeof(int));
 
-        // CUDA Event for timing
         cudaEvent_t start, stop;
         float milliseconds = 0.0f;
         cudaEventCreate(&start);
@@ -112,7 +78,6 @@ int main() {
         cudaEventElapsedTime(&milliseconds, start, stop);
         std::cout << "Time for equalize_image (" << size.first << "x" << size.second << "): " << milliseconds << " ms" << std::endl;
 
-        // Copy result back to host
         unsigned char* h_output = new unsigned char[width * height];
         cudaMemcpy(h_output, d_output, width * height * sizeof(unsigned char), cudaMemcpyDeviceToHost);
 
@@ -122,16 +87,13 @@ int main() {
         cv::imwrite(outputFilePath, outputImg);
         std::cout << "Equalized image saved to " << outputFilePath << std::endl;
 
-        // Free device memory
         cudaFree(d_input);
         cudaFree(d_output);
         cudaFree(d_hist);
         cudaFree(d_cdf);
 
-        // Free host memory
         delete[] h_output;
 
-        // Destroy CUDA events
         cudaEventDestroy(start);
         cudaEventDestroy(stop);
     }
